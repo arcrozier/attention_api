02bea032b2058ece7f81d6f9e61d2bf8fd2e5d58
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2022-01-03 13:22:45 -0800

    Worked on setting up Django project

--- v2/models.py
+++ v2/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 
 
+class Users(models.Model):
+    public_key = models.CharField(max_length=255, primary_key=True)
+    fcm_id = models.TextField(unique=True)
+    challenge = models.CharField(max_length=16)
+    challenge_timeout = models.DateTimeField()
+
--- v2/views.py
+++ v2/views.py
@@ -1,3 +1,28 @@
+import json
+from typing import Any
+
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+
+from v2.models import Users
+
+
+class ApiV2(APIView):
+    queryset = Users.objects.all()
+
+
+def build_response(success: bool, message: str, data: Any = None, string: bool = False) -> dict:
+    response = {
+        "success": success,
+        "message": message,
+        "data": data
+    }
+    if string:
+        response = string_response(response)
+    return response
+
+
+def string_response(args: dict):
+    return json.dumps(args)
