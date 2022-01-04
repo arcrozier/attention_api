62b4b198668296bf36b6c39d9581ff75f0063411
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2022-01-04 12:28:21 -0800

    Moved to function-based views

--- v2/views.py
+++ v2/views.py
@@ -4,13 +4,28 @@ from typing import Any
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from v2.models import Users
 
 
-class ApiV2(APIView):
-    queryset = Users.objects.all()
+@api_view(['GET'])
+def get_challenge(request: Request, public_key: str) -> Response:
+    # TODO: Generate a new challenge
+    pass
+
+
+@api_view(['POST'])
+def upload_public_key(request: Request) -> Response:
+    pass
+
+
+@api_view(['POST'])
+def send_alert(request: Request) -> Response:
+    pass
 
 
 def build_response(success: bool, message: str, data: Any = None, string: bool = False) -> dict:
