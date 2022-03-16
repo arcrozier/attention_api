01d6851d8d87e8908792dbdfcfd9ff31ef5ca396
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2022-03-16 16:15:03 -0700

    Activate venv before starting server

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P
 export DJANGO_SETTINGS_MODULE='attention_api.production'
+source .venv/bin/activate
 hypercorn --bind localhost:8002 attention_api.asgi:application
