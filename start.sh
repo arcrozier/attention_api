71a18d62a0eeec765f7194b26e31356d50fbc8f2
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2022-07-05 00:38:07 -0700

    Modified to use pipenv

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+port=${1:-8002}
+
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P
 export DJANGO_SETTINGS_MODULE='attention_api.production'
-./.venv/bin/python3.9 -m hypercorn --bind localhost:8002 attention_api.asgi:application
+pipenv run python3.9 -m hypercorn --bind localhost:"$port" attention_api.asgi:application
