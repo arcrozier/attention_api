58289af4cc3036bc209fa6e1be4ffd90e2d814a6
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2022-08-13 14:31:39 -0700

    Updated systemd configuration

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P
 export DJANGO_SETTINGS_MODULE='attention_api.production'
-./.venv/bin/python3.9 -m hypercorn --bind localhost:8002 attention_api.asgi:application
+./.venv/bin/python3.10 -m hypercorn --bind localhost:8002 attention_api.asgi:application
