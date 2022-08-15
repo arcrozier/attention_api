e1e8c5f299bcab1a8e4c6763ade54d4ebcdc0718
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2022-08-14 19:09:12 -0700

    Updated pipfile and start script to Python 3.10

--- start.sh
+++ start.sh
@@ -6,4 +6,4 @@ db_credentials=${2:-"/var/local/db_credentials/attention_api.env"}
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P
 export DJANGO_SETTINGS_MODULE='attention_api.production'
 export DB_CREDENTIALS_FILE=$db_credentials
-python3.9 -m pipenv run python3.9 -m hypercorn --bind localhost:"$port" attention_api.asgi:application
+python3.10 -m pipenv run python3.10 -m hypercorn --bind localhost:"$port" attention_api.asgi:application
