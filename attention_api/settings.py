3c23bcf56fbe0150cb3365aa61f53c2e8cf0e2ce
Aragorn Crozier <45837343+arcrozier@users.noreply.github.com>
2022-09-04 14:56:28 -0700

    Experimenting with django-rest-knox

--- attention_api/settings.py
+++ attention_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['ATTENTION_API_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.2.2']
 
 
 # Application definition
